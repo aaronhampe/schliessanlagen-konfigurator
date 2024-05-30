96cc6c9e5d12ef3010ab52be1291230f1b321910
Nicolaus78 <fb@secutimetec.de>
2024-05-30 09:42:49 +0200

    fff

--- server/api/sqlpostanlageneu.ts
+++ server/api/sqlpostanlageneu.ts
@@ -15,7 +15,14 @@ export default defineEventHandler(async (event) => {
 
     const queryresult = await prisma.$queryRaw`
     INSERT INTO  Anlage (ID, Objekt, Name, Vorname, EMail, Firma)
-    VALUES (${ID}, ${Objekt}, ${Name}, ${Vorname}, ${EMail}, ${Firma});`;
+    VALUES (${ID}, ${Objekt}, ${Name}, ${Vorname}, ${EMail}, ${Firma})
+    ON DUPLICATE KEY UPDATE
+    Objekt = VALUES(Objekt),
+    Name = VALUES(Name),
+    Vorname = VALUES(Vorname),
+    EMail = VALUES(EMail),
+    Firma = VALUES(Firma);
+    ;`;
 
   return {
     queryresult
