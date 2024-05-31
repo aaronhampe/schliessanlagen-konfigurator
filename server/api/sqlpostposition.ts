901493dde7ad699aab27b236eab5c62408dd54d7
Nicolaus78 <fb@secutimetec.de>
2024-05-31 13:30:13 +0200

    ggggg

--- server/api/sqlpostposition.ts
+++ server/api/sqlpostposition.ts
@@ -13,20 +13,30 @@ export default defineEventHandler(async (event) => {
   
 
     async function insertData() {
+      //console.log(body);
       const insertQueries = body.map(item => {
-        return
-        prisma.$queryRaw`INSERT INTO deine_tabelle_name (ID, POS, Bezeichnung, Anzahl, Typ, SizeA, SizeI, Option) VALUES (${item.ID}, ${item.POS}, ${item.Bezeichnung}, ${item.Anzahl}, ${item.Typ}, ${item.SizeA}, ${item.SizeI}, ${item.Option})`;
-      });
+        return prisma.$queryRaw`INSERT INTO Position (ID, POS, Bezeichnung, Anzahl, Typ, SizeA, SizeI, Option) 
+                             VALUES (${item.ID}, ${item.POS}, ${item.Bezeichnung}, ${item.Anzahl}, ${item.Typ}, ${item.SizeA}, ${item.SizeI}, ${item.Option}) 
+                             ON DUPLICATE KEY UPDATE 
+                             Bezeichnung = ${item.Bezeichnung}, 
+                             Anzahl = ${item.Anzahl}, 
+                             Typ = ${item.Typ}, 
+                             SizeA = ${item.SizeA}, 
+                             SizeI = ${item.SizeI}, 
+                             Option = ${item.Option}`;
+  });
+  console.log(insertQueries);
     
-      await Promise.all(insertQueries);
+     await Promise.all(insertQueries);
     }
     insertData()
-    .catch(e => {
+    
+    /* .catch(e => {
       console.error(e);
     })
     .finally(async () => {
       await prisma.$disconnect();
-    }); 
+    });  */
 
   return {
     
