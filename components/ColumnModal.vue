f7ef8da8fe4014d06151afe4706e2762eadba00f
aaronthom <hampeaaron@gmail.com>
2024-06-28 11:57:50 +0200

    autofocus

--- components/ColumnModal.vue
+++ components/ColumnModal.vue
@@ -6,7 +6,7 @@
                 <UButton color="red" @click="closeModal" style="font-weight: 600;">X</UButton>
             </div>
             <label for="input">Schlüsselbezeichnung:</label>
-            <UInput class="input pb-6 z-10" v-model="columnName" color="amber" />
+            <UInput autofocus class="input pb-6 z-10" v-model="columnName" color="amber" />
             <UButton class="button" @click="closeModal" color="amber">Speichern</UButton>
         </div>
     </UModal>
