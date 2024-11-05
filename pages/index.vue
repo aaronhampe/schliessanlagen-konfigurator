0f0e963322855139abda28eb4af93beeaab059ba
aaronthom <hampeaaron@gmail.com>
2024-11-05 21:50:18 +0100

    progress

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
     <!-- <NewConfigurator :isSchliessanlage="$route.query.isSchliessanlage === 'true'" /> -->
-    <ConfiguratorView :isSchliessanlage="true" />
+    <!-- <ConfiguratorView :isSchliessanlage="true" /> -->
+    <CompAPI :isSchliessanlage="true" />
+    
 </template>
   
 
 <script setup>
+import CompAPI from '~/components/compAPI.vue';
+
 </script>
