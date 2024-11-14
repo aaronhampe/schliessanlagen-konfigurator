ef1895b32df5fafa30f21ba74fa88c9d7d60ede1
aaronthom <hampeaaron@gmail.com>
2024-11-14 14:10:35 +0100

    changes

--- data/cylinderModels.js
+++ data/cylinderModels.js
@@ -123,5 +123,73 @@ export default {
             "Vorhangschloss 80mm",
             "Briefkastenschloss"
         ]
+    },
+    "ABUS EC660": {
+        Doppelzylinder: {
+            sizes: [
+                { inside: 30, outside: 30 },
+                { inside: 30, outside: 35 },
+                { inside: 30, outside: 40 },
+                { inside: 30, outside: 45 },
+                { inside: 35, outside: 40 },
+                { inside: 40, outside: 45 },
+                { inside: 30, outside: 40 },
+                { inside: 30, outside: 45 },
+                { inside: 35, outside: 40 },
+                { inside: 40, outside: 45 },
+            ],
+            options: {
+                finishes: ["Nickel", "Bronze", "Brass"],
+                security: ["Standard", "High Security"]
+            }
+        },
+        Knaufzylinder: {
+            sizes: [
+                { inside: 30, outside: 35 },
+                { inside: 35, outside: 40 }
+            ],
+            options: {
+                knobStyles: ["Round", "Oval"],
+                finishes: ["Nickel", "Bronze"]
+            }
+        },
+        Halbzylinder: {
+            sizes: [
+                { inside: 30, outside: 10 },
+                { inside: 35, outside: 10 }
+            ],
+            options: {
+                finishes: ["Nickel", "Bronze"],
+               
+            }
+        },
+        Vorhangschloss: {
+            types: {
+                "50mm": {
+                    options: {
+                        shackleHeight: ["Standard", "Long"]
+                    }
+                },
+                "80mm": {
+                    options: {
+                        shackleHeight: ["Standard", "Long", "Extra Long"]
+                    }
+                }
+            }
+        },
+        Briefkastenschloss: {
+            options: {
+                mountingTypes: ["Surface", "Mortise"],
+                finishes: ["Nickel", "Bronze"]
+            }
+        },
+        availableTypes: [
+            "Doppelzylinder",
+            "Knaufzylinder (innen)",
+            "Halbzylinder",
+            "Vorhangschloss 50mm",
+            "Vorhangschloss 80mm",
+            "Briefkastenschloss"
+        ]
     }
 };
