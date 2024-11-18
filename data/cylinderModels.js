c0856c0bf1273d1db0b11b21f26132bb9634c12a
Aaron Hampe <hampeaaron@gmail.com>
2024-11-18 12:11:58 +0100

    changes so far, now options problem

--- data/cylinderModels.js
+++ data/cylinderModels.js
@@ -50,6 +50,58 @@ export default {
           { inside: 30, outside: 40 },
           { inside: 30, outside: 45 },
           { inside: 30, outside: 50 },
+          { inside: 35, outside: 35 },
+          { inside: 35, outside: 40 },
+          { inside: 35, outside: 45 },
+          { inside: 40, outside: 40 },
+          { inside: 40, outside: 45 },
+          { inside: 45, outside: 45 },
+          { inside: 35, outside: 40 },
+          { inside: 40, outside: 45 },
+          { inside: 50, outside: 30 },
+        ],
+        options: ["Nickel", "Bronze", "Brass", "Standard", "High Security", "Not- & Gefahrenfunktion"],
+      },
+      Knaufzylinder: {
+        sizes: [
+          { inside: 30, outside: 35 },
+          { inside: 35, outside: 40 },
+        ],
+        options: ["Round", "Oval", "Nickel", "Bronze", "Not- & Gefahrenfunktion"],
+      },
+      Halbzylinder: {
+        sizes: [
+          { inside: 30, outside: 0 },
+          { inside: 35, outside: 0 },
+        ],
+        options: ["Nickel", "Bronze", "Standard", "Security"],
+      },
+      Vorhangschloss: {
+        types: {
+          "50mm": { options: ["Standard", "Long"] },
+          "80mm": { options: ["Standard", "Long", "Extra Long"] },
+        },
+      },
+      Briefkastenschloss: {
+        options: ["Surface", "Mortise", "Nickel", "Bronze"],
+      },
+      availableTypes: [
+        "Doppelzylinder",
+        "Knaufzylinder (innen)",
+        "Halbzylinder",
+        "Vorhangschloss 50mm",
+        "Vorhangschloss 80mm",
+        "Briefkastenschloss",
+      ],
+    },
+    "ABUS EC660": {
+      Doppelzylinder: {
+        sizes: [
+          { inside: 30, outside: 30 },
+          { inside: 30, outside: 35 },
+          { inside: 30, outside: 40 },
+          { inside: 30, outside: 45 },
+          { inside: 30, outside: 50 },
           
           { inside: 35, outside: 35 },
           { inside: 35, outside: 40 },
