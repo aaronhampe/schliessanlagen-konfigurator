e12ff0182351e186e02351556b8d91623548c4bd
Aaron Hampe <hampeaaron@gmail.com>
2024-11-25 13:15:58 +0100

    changes

--- data/cylinderModels.js
+++ data/cylinderModels.js
@@ -150,5 +150,49 @@ export default {
         "Briefkastenschloss",
       ],
     },
+    "ABUS EC880": {
+      Doppelzylinder: {
+        sizes: [
+          
+          { inside: 50, outside: 30 },
+        ],
+        options: {
+          Material: ["Nickel", "Bronze", "Brass"],
+          Sicherheit: ["Standard", "High Security", "Not- & Gefahrenfunktion"],
+          Finish: ["Matt", "Poliert"],
+        },
+      },
+      Knaufzylinder: {
+        sizes: [
+          { inside: 30, outside: 35 },
+          { inside: 35, outside: 40 },
+        ],
+        options: ["Round", "Oval", "Nickel", "Bronze", "Not- & Gefahrenfunktion"],
+      },
+      Halbzylinder: {
+        sizes: [
+          { inside: 30, outside: 0 },
+          { inside: 35, outside: 0 },
+        ],
+        options: ["Nickel", "Bronze", "Standard", "Security"],
+      },
+      Vorhangschloss: {
+        types: {
+          "50mm": { options: ["Standard", "Long"] },
+          "80mm": { options: ["Standard", "Long", "Extra Long"] },
+        },
+      },
+      Briefkastenschloss: {
+        options: ["Surface", "Mortise", "Nickel", "Bronze"],
+      },
+      availableTypes: [
+        "Doppelzylinder",
+        "Knaufzylinder (innen)",
+        "Halbzylinder",
+        "Vorhangschloss 50mm",
+        "Vorhangschloss 80mm",
+        "Briefkastenschloss",
+      ],
+    },
   };
   
