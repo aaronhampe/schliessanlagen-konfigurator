5babfc81cd534529d657173893cc7a825676b24e
aaronthom <hampeaaron@gmail.com>
2025-01-23 17:58:38 +0100

    data changes

--- data/models/abus/ec660.js
+++ data/models/abus/ec660.js
@@ -1,192 +1,180 @@
 export default {
-    isSchliessanlage: false,
-    image: "/images/abus-ec660-doppelzylinder-logo-500x500.png",
-  
-    Doppelzylinder: {
-      sizes: [
-    
-        { outside: 34, inside: 28 },
-        { outside: 39, inside: 28 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 30 },
-        { outside: 35, inside: 30 },
-        { outside: 40, inside: 30 },
-        { outside: 45, inside: 30 },
-        { outside: 50, inside: 30 },
-        { outside: 55, inside: 30 },
-        { outside: 60, inside: 30 },
-        { outside: 65, inside: 30 },
-        { outside: 70, inside: 30 },
-        { outside: 80, inside: 30 },
-        { outside: 90, inside: 30 },
-        { outside: 100, inside: 30 },
-        { outside: 110, inside: 30 },
-        { outside: 120, inside: 30 },
-  
-        ////////////////////////////////////////////
-        { outside: 35, inside: 35 },
-        { outside: 40, inside: 35 },
-        { outside: 45, inside: 35 },
-        { outside: 50, inside: 35 },
-        { outside: 55, inside: 35 },
-        { outside: 60, inside: 35 },
-        { outside: 65, inside: 35 },
-        { outside: 70, inside: 35 },
-  
-        ////////////////////////////////////////////
-        { outside: 40, inside: 40 },
-        { outside: 45, inside: 40 },
-        { outside: 50, inside: 40 },
-        { outside: 55, inside: 40 },
-        { outside: 60, inside: 40 },
-        { outside: 65, inside: 40 },
-        { outside: 70, inside: 40 },
-  
-        ////////////////////////////////////////////
-        { outside: 45, inside: 45 },
-        { outside: 50, inside: 45 },
-        { outside: 55, inside: 45 },
-        { outside: 60, inside: 45 },
-        { outside: 65, inside: 45 },
-        { outside: 70, inside: 45 },
-  
-        ////////////////////////////////////////////
-        { outside: 50, inside: 50 },
-        { outside: 55, inside: 50 },
-        { outside: 60, inside: 50 },
-        { outside: 65, inside: 50 },
-        { outside: 70, inside: 50 },
-  
-        ////////////////////////////////////////////
-        { outside: 55, inside: 55 },
-        { outside: 60, inside: 55 },
-        { outside: 65, inside: 55 },
-      ],
-      options: [],
-    },
-  
-    Knaufzylinder: {
-      sizes: [
-        
-        { outside: 34, inside: 28 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 30 },
-        { outside: 35, inside: 30 },
-        { outside: 40, inside: 30 },
-        { outside: 45, inside: 30 },
-        { outside: 50, inside: 30 },
-        { outside: 55, inside: 30 },
-        { outside: 60, inside: 30 },
-        { outside: 65, inside: 30 },
-        { outside: 70, inside: 30 },
-        { outside: 80, inside: 30 },
-        { outside: 90, inside: 30 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 35 },
-        { outside: 35, inside: 35 },
-        { outside: 40, inside: 35 },
-        { outside: 45, inside: 35 },
-        { outside: 50, inside: 35 },
-        { outside: 55, inside: 35 },
-        { outside: 60, inside: 35 },
-        { outside: 65, inside: 35 },
-        { outside: 70, inside: 35 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 40 },
-        { outside: 35, inside: 40 },
-        { outside: 40, inside: 40 },
-        { outside: 45, inside: 40 },
-        { outside: 50, inside: 40 },
-        { outside: 55, inside: 40 },
-        { outside: 60, inside: 40 },
-        { outside: 65, inside: 40 },
-        { outside: 70, inside: 40 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 45 },
-        { outside: 35, inside: 45 },
-        { outside: 40, inside: 45 },
-        { outside: 45, inside: 45 },
-        { outside: 50, inside: 45 },
-        { outside: 55, inside: 45 },
-        { outside: 60, inside: 45 },
-        { outside: 65, inside: 45 },
-        { outside: 70, inside: 45 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 50 },
-        { outside: 35, inside: 50 },
-        { outside: 40, inside: 50 },
-        { outside: 45, inside: 50 },
-        { outside: 50, inside: 50 },
-        { outside: 55, inside: 50 },
-        { outside: 60, inside: 50 },
-        { outside: 65, inside: 50 },
-        { outside: 70, inside: 50 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 55 },
-        { outside: 35, inside: 55 },
-        { outside: 40, inside: 55 },
-        { outside: 45, inside: 55 },
-        { outside: 50, inside: 55 },
-        { outside: 55, inside: 55 },
-        { outside: 60, inside: 55 },
-        { outside: 65, inside: 55 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 60 },
-        { outside: 35, inside: 60 },
-        { outside: 40, inside: 60 },
-        { outside: 45, inside: 60 },
-        { outside: 50, inside: 60 },
-        { outside: 55, inside: 60 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 65 },
-        { outside: 35, inside: 65 },
-        { outside: 40, inside: 65 },
-        { outside: 45, inside: 65 },
-        { outside: 50, inside: 65 },
-        { outside: 55, inside: 65 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 70 },
-        { outside: 35, inside: 70 },
-        { outside: 40, inside: 70 },
-        { outside: 45, inside: 70 },
-        { outside: 50, inside: 70 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 80 },
-  
-        ////////////////////////////////////////////
-        { outside: 30, inside: 90 },
-      ],
-      options: [],
-    },
-  
-    Halbzylinder: {
-      sizes: [
-        { outside: 10, inside: 30 },
-        { outside: 10, inside: 35 },
-        { outside: 10, inside: 40 },
-        { outside: 10, inside: 45 },
-        { outside: 10, inside: 50 },
-        { outside: 10, inside: 60 },
-        { outside: 10, inside: 70 },
-      ],
-      options: [],
-    },
-  
-    availableTypes: [
-      "Doppelzylinder",
-      "Knaufzylinder (innen)",
-      "Halbzylinder",
+  isSchliessanlage: false,
+  image: "/images/abus-ec660-doppelzylinder-logo-500x500.png",
+
+  Doppelzylinder: {
+    sizes: [
+      { outside: 28, inside: 34 },
+      { outside: 28, inside: 39 },
+      ////////////////////////////////////////////
+      { outside: 30, inside: 30 },
+      { outside: 30, inside: 35 },
+      { outside: 30, inside: 40 },
+      { outside: 30, inside: 50 },
+      { outside: 30, inside: 55 },
+      { outside: 30, inside: 60 },
+      { outside: 30, inside: 65 },
+      { outside: 30, inside: 70 },
+      { outside: 30, inside: 75 },
+      { outside: 30, inside: 80 },
+      { outside: 30, inside: 85 },
+      { outside: 30, inside: 90 },
+      { outside: 30, inside: 95 },
+      { outside: 30, inside: 100 },
+      { outside: 30, inside: 105 },
+      { outside: 30, inside: 110 },
+      { outside: 30, inside: 115 },
+      { outside: 30, inside: 120 },
+      ////////////////////////////////////////////
+      { outside: 35, inside: 30 },
+      { outside: 35, inside: 35 },
+      { outside: 35, inside: 40 },
+      { outside: 35, inside: 45 },
+      { outside: 35, inside: 50 },
+      { outside: 35, inside: 55 },
+      { outside: 35, inside: 60 },
+      { outside: 35, inside: 65 },
+      { outside: 35, inside: 70 },
+      ////////////////////////////////////////////
+      { outside: 40, inside: 30 },
+      { outside: 40, inside: 35 },
+      { outside: 40, inside: 40 },
+      { outside: 40, inside: 45 },
+      { outside: 40, inside: 50 },
+      { outside: 40, inside: 55 },
+      { outside: 40, inside: 60 },
+      { outside: 40, inside: 65 },
+      { outside: 40, inside: 70 },
+      ////////////////////////////////////////////
+      { outside: 45, inside: 45 },
+      { outside: 45, inside: 50 },
+      { outside: 45, inside: 55 },
+      { outside: 45, inside: 60 },
+      { outside: 45, inside: 65 },
+      { outside: 45, inside: 70 },
+      ////////////////////////////////////////////
+      { outside: 50, inside: 50 },
+      { outside: 50, inside: 55 },
+      { outside: 50, inside: 60 },
+      { outside: 50, inside: 65 },
+      ////////////////////////////////////////////
+      { outside: 55, inside: 55 },
+      { outside: 55, inside: 60 },
+      { outside: 55, inside: 65 },
     ],
-  }
-  
+    options: [],
+  },
+
+  Knaufzylinder: {
+    sizes: [
+
+      { outside: 28, inside: 34 },
+      ////////////////////////////////////////////
+      { outside: 30, inside: 30 },
+      { outside: 30, inside: 35 },
+      { outside: 30, inside: 40 },
+      { outside: 30, inside: 50 },
+      { outside: 30, inside: 55 },
+      { outside: 30, inside: 60 },
+      { outside: 30, inside: 65 },
+      { outside: 30, inside: 70 },
+      { outside: 30, inside: 75 },
+      { outside: 30, inside: 80 },
+      { outside: 30, inside: 85 },
+      { outside: 30, inside: 90 },
+      ////////////////////////////////////////////
+      { outside: 35, inside: 30 },
+      { outside: 35, inside: 35 },
+      { outside: 35, inside: 40 },
+      { outside: 35, inside: 45 },
+      { outside: 35, inside: 50 },
+      { outside: 35, inside: 55 },
+      { outside: 35, inside: 60 },
+      { outside: 35, inside: 65 },
+      { outside: 35, inside: 70 },
+      ////////////////////////////////////////////
+      { outside: 40, inside: 30 },
+      { outside: 40, inside: 35 },
+      { outside: 40, inside: 40 },
+      { outside: 40, inside: 45 },
+      { outside: 40, inside: 50 },
+      { outside: 40, inside: 55 },
+      { outside: 40, inside: 60 },
+      { outside: 40, inside: 65 },
+      { outside: 40, inside: 70 },
+      ////////////////////////////////////////////
+      { outside: 45, inside: 30 },
+      { outside: 45, inside: 35 },
+      { outside: 45, inside: 40 },
+      { outside: 45, inside: 45 },
+      { outside: 45, inside: 50 },
+      { outside: 45, inside: 55 },
+      { outside: 45, inside: 60 },
+      { outside: 45, inside: 65 },
+      { outside: 45, inside: 70 },
+      ////////////////////////////////////////////
+      { outside: 50, inside: 30 },
+      { outside: 50, inside: 35 },
+      { outside: 50, inside: 40 },
+      { outside: 50, inside: 45 },
+      { outside: 50, inside: 50 },
+      { outside: 50, inside: 55 },
+      { outside: 50, inside: 60 },
+      { outside: 50, inside: 65 },
+      { outside: 50, inside: 70 },
+      ////////////////////////////////////////////
+      { outside: 55, inside: 30 },
+      { outside: 55, inside: 35 },
+      { outside: 55, inside: 40 },
+      { outside: 55, inside: 45 },
+      { outside: 55, inside: 50 },
+      { outside: 55, inside: 55 },
+      { outside: 55, inside: 60 },
+      { outside: 55, inside: 65 },
+      ////////////////////////////////////////////
+      { outside: 60, inside: 30 },
+      { outside: 60, inside: 35 },
+      { outside: 60, inside: 40 },
+      { outside: 60, inside: 45 },
+      { outside: 60, inside: 50 },
+      { outside: 60, inside: 55 },
+      ////////////////////////////////////////////
+      { outside: 65, inside: 30 },
+      { outside: 65, inside: 35 },
+      { outside: 65, inside: 40 },
+      { outside: 65, inside: 45 },
+      { outside: 65, inside: 50 },
+      { outside: 65, inside: 55 },
+      ////////////////////////////////////////////
+      { outside: 70, inside: 30 },
+      { outside: 70, inside: 35 },
+      { outside: 70, inside: 40 },
+      { outside: 70, inside: 45 },
+      { outside: 70, inside: 50 },
+      ////////////////////////////////////////////
+      { outside: 80, inside: 30 },
+      ////////////////////////////////////////////
+      { outside: 90, inside: 30 },
+    ],
+    options: [],
+  },
+
+  Halbzylinder: {
+    sizes: [
+      { outside: 10, inside: 30 },
+      { outside: 10, inside: 35 },
+      { outside: 10, inside: 40 },
+      { outside: 10, inside: 45 },
+      { outside: 10, inside: 50 },
+      { outside: 10, inside: 60 },
+      { outside: 10, inside: 70 },
+    ],
+    options: [],
+  },
+
+  availableTypes: [
+    "Doppelzylinder",
+    "Knaufzylinder (innen)",
+    "Halbzylinder",
+  ],
+}
