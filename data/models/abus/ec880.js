9cd051e652285cf89f9537041945a2f7c8a5a4fb
aaronthom <hampeaaron@gmail.com>
2025-01-28 16:40:53 +0100

    data changes

--- data/models/abus/ec880.js
+++ data/models/abus/ec880.js
@@ -3,146 +3,130 @@ export default {
     image: "/images/abus-ec880-doppelzylinder-logo-500x500.png",
     Doppelzylinder: {
         sizes: [
-            { outside: 28, inside: 34 },
-            { outside: 28, inside: 39 },
-            ////////////////////////////////////////////
-            { outside: 30, inside: 30 },
-            { outside: 30, inside: 35 },
-            { outside: 30, inside: 40 },
-            { outside: 30, inside: 50 },
-            { outside: 30, inside: 55 },
-            { outside: 30, inside: 60 },
-            { outside: 30, inside: 65 },
-            { outside: 30, inside: 70 },
-            { outside: 30, inside: 75 },
-            { outside: 30, inside: 80 },
-            { outside: 30, inside: 85 },
-            { outside: 30, inside: 90 },
-            { outside: 30, inside: 95 },
-            { outside: 30, inside: 100 },
-            { outside: 30, inside: 105 },
-            { outside: 30, inside: 110 },
-            { outside: 30, inside: 115 },
-            { outside: 30, inside: 120 },
-            ////////////////////////////////////////////
-            { outside: 35, inside: 30 },
-            { outside: 35, inside: 35 },
-            { outside: 35, inside: 40 },
-            { outside: 35, inside: 45 },
-            { outside: 35, inside: 50 },
-            { outside: 35, inside: 55 },
-            { outside: 35, inside: 60 },
-            { outside: 35, inside: 65 },
-            { outside: 35, inside: 70 },
-            { outside: 35, inside: 75 },
-            { outside: 35, inside: 80 },
-            { outside: 35, inside: 85 },
-            { outside: 35, inside: 90 },
-            { outside: 35, inside: 95 },
-            { outside: 35, inside: 100 },
-            { outside: 35, inside: 105 },
-            { outside: 35, inside: 110 },
-            { outside: 35, inside: 115 },
-            { outside: 35, inside: 120 },
-            ////////////////////////////////////////////
-            { outside: 40, inside: 30 },
-            { outside: 40, inside: 35 },
-            { outside: 40, inside: 40 },
-            { outside: 40, inside: 45 },
-            { outside: 40, inside: 50 },
-            { outside: 40, inside: 55 },
-            { outside: 40, inside: 60 },
-            { outside: 40, inside: 65 },
-            { outside: 40, inside: 70 },
-            { outside: 30, inside: 75 },
-            { outside: 40, inside: 80 },
-            { outside: 40, inside: 85 },
-            { outside: 40, inside: 90 },
-            { outside: 40, inside: 95 },
-            { outside: 40, inside: 100 },
-            { outside: 40, inside: 105 },
-            { outside: 40, inside: 110 },
-            { outside: 40, inside: 115 },
-            { outside: 40, inside: 120 },
-            ////////////////////////////////////////////
-            { outside: 45, inside: 45 },
-            { outside: 45, inside: 50 },
-            { outside: 45, inside: 55 },
-            { outside: 45, inside: 60 },
-            { outside: 45, inside: 65 },
-            { outside: 45, inside: 70 },
-            ////////////////////////////////////////////
-            { outside: 50, inside: 50 },
-            { outside: 50, inside: 55 },
-            { outside: 50, inside: 60 },
-            { outside: 50, inside: 65 },
-            { outside: 50, inside: 70 },
-            ////////////////////////////////////////////
-            { outside: 55, inside: 55 },
-            { outside: 55, inside: 60 },
-            { outside: 55, inside: 65 },
-            { outside: 55, inside: 70 },
-            ////////////////////////////////////////////
-            { outside: 60, inside: 60 },
-            { outside: 60, inside: 65 },
-            { outside: 60, inside: 70 },
-            ////////////////////////////////////////////
-            { outside: 65, inside: 65 },
-            { outside: 65, inside: 70 },
-            ////////////////////////////////////////////
-            { outside: 70, inside: 70 },
+
+            /////////////////
+            { outside: 30, inside: 30, price: 55.60 },
+            { outside: 30, inside: 35, price: 59.80 },
+            { outside: 30, inside: 40, price: 62.95 },
+            { outside: 30, inside: 50, price: 66.10 },
+            { outside: 30, inside: 55, price: 69.25 },
+            { outside: 30, inside: 60, price: 73.45 },
+            { outside: 30, inside: 65, price: 76.60 },
+            { outside: 30, inside: 70, price: 79.75 },
+            { outside: 30, inside: 75, price: 82.90 },
+            { outside: 30, inside: 80, price: 90.25 },
+            { outside: 30, inside: 90, price: 96.55 },
+            { outside: 30, inside: 100, price: 103.90 },
+            { outside: 30, inside: 110, price: 110.20 },
+            { outside: 30, inside: 120, price: 117.55 },
+
+            ////////////////////
+            { outside: 35, inside: 35, price: 62.95 },
+            { outside: 35, inside: 40, price: 66.10 },
+            { outside: 35, inside: 45, price: 69.25 },
+            { outside: 35, inside: 50, price: 73.45 },
+            { outside: 35, inside: 55, price: 76.60 },
+            { outside: 35, inside: 60, price: 79.75 },
+            { outside: 35, inside: 65, price: 82.90 },
+            { outside: 35, inside: 70, price: 87.10 },
+            { outside: 35, inside: 80, price: 92.35 },
+            { outside: 35, inside: 90, price: 100.75 },
+            { outside: 35, inside: 100, price: 107.05 },
+            { outside: 35, inside: 110, price: 114.40 },
+            { outside: 35, inside: 120, price: 120.70 },
+
+            //////////////////
+            { outside: 40, inside: 40, price: 69.25 },
+            { outside: 40, inside: 45, price: 73.45 },
+            { outside: 40, inside: 50, price: 76.60 },
+            { outside: 40, inside: 55, price: 79.75 },
+            { outside: 40, inside: 60, price: 82.90 },
+            { outside: 40, inside: 65, price: 87.10 },
+            { outside: 40, inside: 70, price: 92.25 },
+            { outside: 40, inside: 80, price: 96.55 },
+            { outside: 40, inside: 90, price: 103.90 },
+            { outside: 40, inside: 100, price: 110.20 },
+            { outside: 40, inside: 110, price: 117.55 },
+            { outside: 40, inside: 120, price: 123.85 },
+
+            ///////////////////
+            { outside: 45, inside: 45, price: 76.60 },
+            { outside: 45, inside: 50, price: 79.75 },
+            { outside: 45, inside: 55, price: 82.90 },
+            { outside: 45, inside: 60, price: 87.10 },
+            { outside: 45, inside: 65, price: 92.25 },
+            { outside: 45, inside: 70, price: 96.55 },
+
+            ///////////////////
+            { outside: 50, inside: 50, price: 82.90 },
+            { outside: 50, inside: 55, price: 87.10 },
+            { outside: 50, inside: 60, price: 90.25 },
+            { outside: 50, inside: 65, price: 93.40 },
+            { outside: 50, inside: 70, price: 96.55 },
+
+            ///////////////////
+            { outside: 55, inside: 55, price: 90.25 },
+            { outside: 55, inside: 60, price: 93.40 },
+            { outside: 55, inside: 65, price: 96.55 },
+            { outside: 55, inside: 70, price: 100.75 },
+
+            ///////////////////
+            { outside: 60, inside: 60, price: 96.55 },
+            { outside: 60, inside: 65, price: 100.75 },
+            { outside: 60, inside: 70, price: 103.90 },
+
+            ///////////////////
+            { outside: 65, inside: 65, price: 103.90 },
+            { outside: 65, inside: 70, price: 107.05 },
+
+            //////////////////
+            { outside: 70, inside: 70, price: 110.20 },
         ],
         options: [],
     },
     Knaufzylinder: {
         sizes: [
-            ////////////////////////////////////////////
-            { outside: 30, inside: 30 },
-            { outside: 30, inside: 35 },
-            { outside: 30, inside: 40 },
-            { outside: 30, inside: 50 },
-            { outside: 30, inside: 55 },
-            { outside: 30, inside: 60 },
-            { outside: 30, inside: 65 },
-            { outside: 30, inside: 70 },
-            { outside: 30, inside: 75 },
-            { outside: 30, inside: 80 },
-            { outside: 30, inside: 85 },
-            { outside: 30, inside: 90 },
-            { outside: 30, inside: 95 },
-            { outside: 30, inside: 100 },
-            { outside: 30, inside: 105 },
-            { outside: 30, inside: 110 },
-            { outside: 30, inside: 115 },
-            { outside: 30, inside: 120 },
-            ////////////////////////////////////////////
-            { outside: 35, inside: 40 },
-            { outside: 35, inside: 50 },
-            { outside: 35, inside: 55 },
-            { outside: 35, inside: 60 },
-            { outside: 35, inside: 65 },
-            { outside: 35, inside: 70 },
-            { outside: 35, inside: 75 },
-            { outside: 35, inside: 80 },
-            { outside: 35, inside: 85 },
-            { outside: 35, inside: 90 },
-            { outside: 35, inside: 95 },
-            { outside: 35, inside: 100 },
-            { outside: 35, inside: 105 },
-            { outside: 35, inside: 110 },
-            { outside: 35, inside: 115 },
-            { outside: 35, inside: 120 },
-            ////////////////////////////////////////////
-            { outside: 40, inside: 30 },
-            { outside: 40, inside: 35 },
-            { outside: 40, inside: 40 },
-            { outside: 40, inside: 50 },
-            { outside: 40, inside: 55 },
-            { outside: 40, inside: 60 },
-            { outside: 40, inside: 65 },
-            { outside: 40, inside: 70 },
-            { outside: 40, inside: 75 },
-            { outside: 40, inside: 80 },
+
+            ///////////////////
+            { outside: 30, inside: 30, price: 49.30 },
+            { outside: 30, inside: 35, price: 52.45 },
+            { outside: 30, inside: 40, price: 55.60 },
+            { outside: 30, inside: 50, price: 61.90 },
+            { outside: 30, inside: 55, price: 66.10 },
+            { outside: 30, inside: 60, price: 69.25 },
+            { outside: 30, inside: 65, price: 72.40 },
+            { outside: 30, inside: 70, price: 75.55 },
+            { outside: 30, inside: 80, price: 82.90 },
+            { outside: 30, inside: 90, price: 89.20 },
+            { outside: 30, inside: 100, price: 96.55 },
+            { outside: 30, inside: 110, price: 102.85 },
+            { outside: 30, inside: 120, price: 110.20 },
+
+            ///////////////////
+            { outside: 35, inside: 30, price: 55.60 },
+            { outside: 35, inside: 35, price: 55.60 },
+            { outside: 35, inside: 40, price: 58.75 },
+            { outside: 35, inside: 50, price: 61.90 },
+            { outside: 35, inside: 55, price: 66.10 },
+            { outside: 35, inside: 60, price: 72.40 },
+            { outside: 35, inside: 65, price: 75.55 },
+            { outside: 35, inside: 70, price: 79.75 },
+            { outside: 35, inside: 80, price: 86.05 },
+            { outside: 35, inside: 90, price: 93.40 },
+            { outside: 35, inside: 100, price: 99.70 },
+            { outside: 35, inside: 110, price: 107.05 },
+            { outside: 35, inside: 120, price: 113.35 },
+
+            ///////////////////
+            { outside: 40, inside: 30, price: 55.60 },
+            { outside: 40, inside: 35, price: 58.75 },
+            { outside: 40, inside: 40, price: 61.90 },
+            { outside: 40, inside: 50, price: 69.25 },
+            { outside: 40, inside: 55, price: 72.40 },
+            { outside: 40, inside: 60, price: 75.55 },
+            { outside: 40, inside: 65, price: 79.75 },
+            { outside: 40, inside: 70, price: 82.90 },
+            { outside: 40, inside: 80, price: 82.90  },
             { outside: 40, inside: 85 },
             { outside: 40, inside: 90 },
             { outside: 40, inside: 95 },
@@ -151,7 +135,8 @@ export default {
             { outside: 40, inside: 110 },
             { outside: 40, inside: 115 },
             { outside: 40, inside: 120 },
-            ////////////////////////////////////////////
+
+            ///////////////////
             { outside: 45, inside: 30 },
             { outside: 45, inside: 35 },
             { outside: 45, inside: 40 },
@@ -161,7 +146,8 @@ export default {
             { outside: 45, inside: 60 },
             { outside: 45, inside: 65 },
             { outside: 45, inside: 70 },
-            ////////////////////////////////////////////
+
+            ///////////////
             { outside: 50, inside: 30 },
             { outside: 50, inside: 35 },
             { outside: 50, inside: 40 },
@@ -170,7 +156,8 @@ export default {
             { outside: 50, inside: 55 },
             { outside: 50, inside: 60 },
             { outside: 50, inside: 65 },
-            ////////////////////////////////////////////
+
+            ///////////////
             { outside: 55, inside: 30 },
             { outside: 55, inside: 35 },
             { outside: 55, inside: 40 },
@@ -179,7 +166,8 @@ export default {
             { outside: 55, inside: 55 },
             { outside: 55, inside: 60 },
             { outside: 55, inside: 65 },
-            ////////////////////////////////////////////
+
+            ////////////////
             { outside: 60, inside: 30 },
             { outside: 60, inside: 35 },
             { outside: 60, inside: 40 },
@@ -187,38 +175,46 @@ export default {
             { outside: 60, inside: 50 },
             { outside: 60, inside: 55 },
             { outside: 60, inside: 60 },
-            ////////////////////////////////////////////
+
+            ///////////////
             { outside: 65, inside: 30 },
             { outside: 65, inside: 35 },
             { outside: 65, inside: 40 },
             { outside: 65, inside: 45 },
             { outside: 65, inside: 50 },
-            ////////////////////////////////////////////
+
+            ///////////////
             { outside: 70, inside: 30 },
             { outside: 70, inside: 35 },
             { outside: 70, inside: 40 },
             { outside: 70, inside: 45 },
-            ////////////////////////////////////////////
+
+            ////////////////
             { outside: 80, inside: 30 },
             { outside: 80, inside: 35 },
             { outside: 80, inside: 40 },
-            ////////////////////////////////////////////
+
+            ///////////////
             { outside: 90, inside: 30 },
             { outside: 90, inside: 35 },
             { outside: 90, inside: 40 },
-            ////////////////////////////////////////////
+
+            //////////////
             { outside: 100, inside: 30 },
             { outside: 100, inside: 35 },
             { outside: 100, inside: 40 },
-            ////////////////////////////////////////////
+
+            //////////////
             { outside: 110, inside: 30 },
             { outside: 110, inside: 35 },
             { outside: 110, inside: 40 },
-            ////////////////////////////////////////////
+
+            /////////////////
             { outside: 120, inside: 30 },
             { outside: 120, inside: 35 },
             { outside: 120, inside: 40 },
-            ////////////////////////////////////////////
+
+            //////////////
         ],
         options: [],
     },
