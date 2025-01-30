9ad44e83b42caa276bdf26569a5b2e0c924f93e8
Nicolaus78 <fb@secutimetec.de>
2025-01-30 15:46:21 +0100

    sdfsfd

--- pages/systeme.vue
+++ pages/systeme.vue
@@ -198,7 +198,8 @@ function addToCart(systemName, price, productID) {
         const cartUrl = result.data.cart_url || "https://www.stt-shop.de/warenkorb/";
         const cartKey = result.data.cart_key;
         const finalUrl = cartKey ? `${cartUrl}?cocart-load-cart=${cartKey}` : cartUrl;
-        window.location.href = finalUrl;
+        //window.location.href = finalUrl;
+        window.open(finalUrl, '_blank');
       }
     })
     .catch((err) => {
