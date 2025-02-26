49223524c025c45666ad0a1fea03cac39c48c61a
Aaron Hampe <hampeaaron@gmail.com>
2025-02-26 16:21:19 +0100

    changes

--- server/plugins/cors.ts
+++ server/plugins/cors.ts
@@ -1,12 +1,9 @@
-import cors from 'cors'
-
 export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook('nitro:config', (config) => {
-    config.plugins = config.plugins || []
-    config.plugins.push((app) => {
-      // Hier wird CORS für alle Endpunkte aktiviert. 
-      // Du kannst die Optionen (z.B. origin) natürlich anpassen.
-      app.use(cors({ origin: '*' }))
-    })
-  })
-})
+    nitroApp.hooks.hook('nitro:config', (config) => {
+      config.plugins = config.plugins || [];
+      config.plugins.push((app) => {
+        app.use(cors({ origin: '*' }));
+      });
+    });
+  });
+  
