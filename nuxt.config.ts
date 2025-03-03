7ee3788f2e658030b5cd26116762c5d1bc49c1dd
Nicolaus78 <fb@secutimetec.de>
2025-03-03 11:04:15 +0100

    xxcvc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   
   app: { 
-    baseURL: '/konfiguratorapp/', 
+    baseURL: '/konfiguratorapp', 
     cdnURL: 'https://www.stt-shop.de/konfiguratorapp/' 
   },
 
