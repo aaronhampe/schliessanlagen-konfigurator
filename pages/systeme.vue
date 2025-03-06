1b26c3d6ac560361366c3d54cfa027beefc7da4c
Nicolaus78 <fb@secutimetec.de>
2025-03-06 14:34:50 +0100

    sfsdgfdg

--- pages/systeme.vue
+++ pages/systeme.vue
@@ -329,12 +329,25 @@ function addToCart(systemName, price, productID) {
 
   $fetch('https://www.stt-shop.de/wp-json/custom/v1/add_to_cart', {
     method: "POST",
-    //
     headers: {
       "Content-Type": "application/json",
-    },
-    
-    body: JSON.stringify({}),
+    },    
+    body: JSON.stringify({
+      product_id: product_id,
+      anlage_nummer: anlageNr,
+      price: price,
+      quantity: 1,
+      config_text: fullConfiguration,
+      widerruf_accepted: true,
+      billing_first_name: body.billing_first_name,
+      billing_last_name: body.billing_last_name,
+      billing_address_1: body.billing_address_1,
+      billing_city: body.billing_city,
+      billing_postcode: body.billing_postcode,
+      billing_country: body.billing_country,
+      billing_email: body.billing_email
+
+    }),
   })
     .then((r) => r.json())
     .then((result) => {
